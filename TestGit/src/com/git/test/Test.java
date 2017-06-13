52291fd178bd5f48ae196a2d7f4fbf901b4ba7f1
yuanjuntao <yuanjuntao@360.cn>
2017-06-13 11:02:54 +0800

    test

--- TestGit/src/com/git/test/Test.java
+++ TestGit/src/com/git/test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("STW");
-		System.out.println("STW222222");
+		System.out.println("STW222");
+		System.out.println("STW---dev");
 	}
 
 }
